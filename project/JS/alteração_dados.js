c39bc463d2202a486e15d672a748105d65956596
Jamil Salomão <132591829+jamilsalomao@users.noreply.github.com>
2024-10-11 12:44:04 -0300

    Atualizações no Front end
    Adicionado pagina de alteração de dados, modificações em botões na landing page e alterações de design em algumas paginas

---
+++ project/JS/alteração_dados.js
@@ -0,0 +1,15 @@
+const validarCampo = (campo) => {
+    const regex = /^[a-zA-Z]+$/; // Permite apenas letras
+    if (event.key !== 'Backspace' && event.key !== 'Delete' && !regex.test(campo.value)) {
+        alert('O campo digitado deve conter apenas letras. Por favor, verifique e tente novamente.');
+        campo.value = campo.value.replace(/\d/g, ''); // Remove números
+    }
+}
+
+const validarCampoNumerico = (campo) => {
+    const regex = /^[0-9]+$/; // Permite apenas números
+    if (event.key !== 'Backspace' && event.key !== 'Delete' && !regex.test(campo.value)) {
+        alert('O campo digitado deve conter apenas números. Por favor, verifique e tente novamente.');
+        campo.value = campo.value.replace(/\D/g, ''); // Remove não-números
+    }
+}
